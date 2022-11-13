aa5b8b94e3cbd9c19e1f54d7b1ceeaa6e35b188b
HAO BAI <haob2@hybrid.ncsa.illinois.edu>
2022-11-13 03:22:16 -0600

    all

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import torch
 import logging
 from module import * 
 from model_utils import *
+from reranker import *
 # '''
 #     Helper Functions/classes
 # '''
@@ -15,7 +16,6 @@ class ret_gen_model():
         super(ret_gen_model,self).__init__()
         self.device = device
         self.dataset = load_from_disk(dataset_path)
-        # self.gen_model = seq2seq_model(gen_model_path,self.device)
         if(model_name == "opt"):
             self.gen_model = opt_model(gen_model_path,self.device)
         else:
@@ -55,16 +55,12 @@ class ret_gen_model():
     
     def gen_response(self,input_q,context):
         prompt = "Answer question from context\nContext: "+context +"\nQuestion: " + input_q + "\nAnswer:"
-        out_ans = self.gen_model.text_gen(prompt,250).split("\n")[3]
+        out_ans = self.gen_model.text_gen(prompt,max_len=250).split("\n")[3]
         return out_ans
     
     def gen_response_list(self,input_q,context_list):
         out_list = []
         for i in range(len(context_list)):
-            # prompt = "Answer question from context\nContext: "+context_list[i].replace("\n"," ") +"\nQuestion: " + input_q + "\nAnswer:"
-            # # out_ans = self.gen_model.text_gen(prompt,200).split("\n")[3]
-            # out_ans = self.gen_model.generate(prompt,200)
-            # out_list.append(out_ans)
             out_ans = self.gen_model.answer_question(context_list[i].replace("\n"," "),input_q.replace("\n"," "))
             out_list.append(out_ans)
         return out_list 
@@ -79,13 +75,20 @@ class ret_gen_model():
     def show_list_result(self,user_utter):
         psg_list = self.ret_psg_list(user_utter)
         ans_list = self.gen_response_list(user_utter,psg_list)
+        scr_list = re_ranker(user_utter, ans_list)
         print("[PASSAGE]: \n")
         for i in range(len(psg_list)):
+            print("-----Passage " + str(i) + "-----\n")
             print(psg_list[i] + "\n")
         print("[RESPONSE]: \n")
         for j in range(len(ans_list)):
+            print("-----Answer " + str(j) + " Score: " + str(scr_list[j]) + "-----\n")
             print(ans_list[j] + "\n")
-        return 
+        print("[Best Response]: \n")
+        # scr_list is a Tensor
+        best_idx = torch.argmax(scr_list)
+        print(ans_list[best_idx] + "\n")
+        return
     
     def qr(self,question,queue:his_queue):
         history_list = queue.get_list()
@@ -128,17 +131,7 @@ class ret_gen_model():
             
         print("[INFO] End Session\n")
 
-
-
-
 if __name__ == "__main__":
-    # my_ret = ret_gen_model(dataset_path = "/home/wentaoy4/lgm/data/convert_dataset/ece_rag_dataset_new/squad-dataset/", index_path = "/home/wentaoy4/lgm/data/convert_dataset/ece_rag_dataset_new/squad-dataset.faiss")
-    # my_ret.get_result("/home/wentaoy4/lgm/qa.csv",delimiter = "?",logger_path = "/home/wentaoy4/lgm/data/logger/metrics_ret_gen.log")
-    # my_ret.get_gpt_dataset_result(json_path = "/home/wentaoy4/UIUC_chatbot_data_generator/Fine_Tuned_Data.json",logger_path="/home/wentaoy4/lgm/data/logger/metrics_ret_gen_gptdata.log")
-    # my_ret.get_gpt_dataset_result(json_path = "/home/wentaoy4/UIUC_chatbot_data_generator/GPT-3_paragraphs.json",logger_path="/home/wentaoy4/lgm/data/logger/metrics_gen_only.log")
-   
-    # my_benchmark = benchmark_gptj("/home/wentaoy4/lgm/data/model_file/EleutherAI--gpt-j-6B.main.918ad376364058dee23512629bc385380c98e57d/")
-    # my_benchmark.eval("/home/wentaoy4/lgm/data/logger/gptj-coqa.log")
     args = main_arg_parse()
     my_chatbot = ret_gen_model(
         model_name=args.model_name,
@@ -150,17 +143,3 @@ if __name__ == "__main__":
         device = args.device
     )
     my_chatbot.odqa_chatbot()
-    # my_device = torch.device("cuda:1")
-    # my_chatbot = ret_gen_model(model_name = "t5",
-    #                            dataset_path = "./dataset/ece_rag_dataset_new/squad-dataset/", 
-    #                            index_path = "./dataset/ece_rag_dataset_new/squad-dataset.faiss",
-    #                            gen_model_path = "/raid/projects/wentaoy4/model_file/models--google--flan-t5-large/snapshots/f5b192378f2e16fb61561ee418736e8c6841c4c8/",
-    #                            # gen_model_path = "/raid/projects/wentaoy4/model_file/models--facebook--opt-1.3b/snapshots/c8fd4232a5df1e87e06d5cbb9e066c5a114cd4ee/",
-    #                            gen_cp_path = "/raid/projects/wentaoy4/model_weight/t5_finetune_b128_e10_lr5e06.pt",
-    #                            # gen_cp_path = "/raid/projects/wentaoy4/model_weight/opt_finetune_b128_e10_lr5e06.pt",
-    #                            # gen_cp_path = None,
-    #                            # logger_path = "/raid/projects/wentaoy4/log/chatbot_opt_finetune.log",
-    #                            logger_path = None,
-    #                            device=my_device)
-    my_chatbot.odqa_chatbot()
-    # my_chatbot.cqa_chatbot()
--- run_chatbot.sh
+++ run_chatbot.sh
@@ -1,6 +1,7 @@
 python main.py \
-    --model_name "opt" \
-    --dataset_path "/home/wentaoy4/lgm/data/convert_dataset/ece_rag_dataset_new/squad-dataset/" \
-    --index_path "/home/wentaoy4/lgm/data/convert_dataset/ece_rag_dataset_new/squad-dataset.faiss" \
-    --gen_model_path "facebook/opt-1.3b" \
+    --model_name "t5" \
+    --dataset_path "/home/haob2/taqa/retreival-generation-system/dataset/ece_rag_dataset_new/squad-dataset/" \
+    --index_path "/home/haob2/taqa/retreival-generation-system/dataset/ece_rag_dataset_new/squad-dataset.faiss" \
+    --gen_model_path "google/flan-t5-large" \
     --device "cuda:0"
+    # --gen_cp_path "/home/wentaoy4/lgm/data/model_weight/opt_finetune_b128_e20_lr5e06.pt" \
